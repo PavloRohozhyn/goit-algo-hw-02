336fc787baa49b7aca8ed4b3a3448731ae1cd1e1
Pavlo Rohozhyn <pavlo.rohozhyn@gmail.com>
2024-08-19 23:06:04 +0200

    task 2 main fuctionale

--- task2.py
+++ task2.py
@@ -1,1 +1,32 @@
-# test
+from collections import deque
+
+def palindrome(data):
+    """ processor """
+
+    # do lowercase
+    data = ''.join(data.lower().split())
+    # add data to enqueu (doble)
+    char_deque = deque(data)
+    # check chars
+    while len(char_deque) > 1:
+        left = char_deque.popleft()
+        right = char_deque.pop()
+        if left != right:
+            return False
+    return True
+
+
+def main():
+    """ Main funciton """
+    # text
+    texts = [
+        'data',
+        'datad',
+        'dataData',
+        'dataDataD'
+    ]
+    for t in texts:
+        print(palindrome(t))
+
+if __name__ == "__main__":
+    main()
